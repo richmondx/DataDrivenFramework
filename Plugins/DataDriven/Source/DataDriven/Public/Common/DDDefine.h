eaf7e9a41b2cdf37bb4bea1896fc64672a4eef2f
liangfuhou <liangfuhou@DESKTOP-0H1NSMO>
2018-05-28 23:11:40 +0800

    公共宏定义文件

---
+++ Plugins/DataDriven/Source/DataDriven/Public/Common/DDDefine.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "DDDefine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATADRIVEN_API UDDDefine : public UObject
+{
+	GENERATED_BODY()
+	
+
+};
+
+
